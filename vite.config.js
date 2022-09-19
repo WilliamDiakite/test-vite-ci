ba7bc4d5dafda3f157d6e01b07a144647ad24d95
William Diakite <william.diakite@gmail.com>
2022-09-19 16:03:27 -0400

    fix base for production

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig((command, mode) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log('app is in', env.NODE_ENV, 'mode')
+
   if (env.NODE_ENV === 'production')
     return {
+      base: '/test-vite-ci/',
       build: {
         rollupOptions: {
           input: {
